c42ce7f0b0e09158b07e3a1d1599e058002ed0b8
N8Brooks <natebrooks004@gmail.com>
2023-02-05 10:49:50 -0600

    add plane data to data transfer

--- src/data_transfer.rs
+++ src/data_transfer.rs
@@ -6,6 +6,36 @@ pub enum Direction {
     Down,
 }
 
+impl Direction {
+    fn as_plane(&self) -> Plane {
+        match self {
+            Direction::Right => Plane::Horizontal,
+            Direction::Up => Plane::Vertical,
+            Direction::Left => Plane::Horizontal,
+            Direction::Down => Plane::Vertical,
+        }
+    }
+}
+
+#[cfg(test)]
+mod direction_tests {
+    use super::*;
+
+    #[test]
+    fn as_plane() {
+        assert_eq!(Direction::Right.as_plane(), Plane::Horizontal);
+        assert_eq!(Direction::Up.as_plane(), Plane::Vertical);
+        assert_eq!(Direction::Left.as_plane(), Plane::Horizontal);
+        assert_eq!(Direction::Down.as_plane(), Plane::Vertical);
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Plane {
+    Horizontal,
+    Vertical,
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Cell {
     Empty,
